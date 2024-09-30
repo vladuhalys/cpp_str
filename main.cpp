1ee27cbe276df44eefb71da330e16faef641caef
Vladyslav Lysenko <vlad.lysenko1999@gmail.com>
2024-09-30 18:30:23 +0300

    Added text

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std;
 
-
+//Build started at 18:23...
+//1 > ------Build started : Project: cpp_str, Configuration : Debug x64------
+//1 > main.cpp
+//1 > cpp_str.vcxproj->C:\Users\lysenko_v\source\repos\cpp_str\x64\Debug\cpp_str.exe
+//========== Build: 1 succeeded, 0 failed, 0 up - to - date, 0 skipped ==========
+//========== Build completed at 18 : 23 and took 03, 045 seconds ==========
 
 int main()
 {
