49072597b6d6dbbaf83459711d585feb1100efd7
Vladyslav Lysenko <vlad.lysenko1999@gmail.com>
2024-09-30 18:26:53 +0300

    Ex 1 from HM was done

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 using namespace std;
 
+class MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+
+private:
+
+};
+
+MyClass::MyClass()
+{
+}
+
+MyClass::~MyClass()
+{
+}
+
 int main()
 {
-
+	MyClass* m = new MyClass();
 }
